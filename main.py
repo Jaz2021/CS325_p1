0b78b35b8730f59b553dcf99479fb331fa697931
J Z <jazeb2021@gmail.com>
2023-09-19 09:38:43 -0500

    Changed to reddit in output and default link

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import argparse
 import requests
 
 parser = argparse.ArgumentParser()
-parser.add_argument("link", default="google.com", nargs="?")
+parser.add_argument("link", default="old.reddit.com/r/funny/comments/16brnzb/self_aware", nargs="?")
 args = parser.parse_args()
 link = args.link
 if (not (link.startswith("http://") or link.startswith("https://"))):
     link = "http://" + link
 page = requests.get(link)
+# This will almost always give me an error for too many requests in the output.txt
 
 file = open("./output.txt", "w")
+
 file.writelines(page.text)
 file.close()
