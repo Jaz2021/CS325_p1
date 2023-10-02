c8f09629c925856bfcad3d9b33e0d8079c8d2d27
J Z <jazeb2021@gmail.com>
2023-10-02 10:36:44 -0500

    Updated to pull comments, now using praw

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # Software Engineering Project 1
 
-Software Engineering Project 1 is an example project meant to scrape a website and output the html into a file
+Software Engineering Project 1 is an example project meant to scrape reddit and output contents to a file. In this, I used praw (the reddit api)
 
 ## Installation and Usage
-1. Create a new conda environment, ex:
-```bash
-conda create --name web_scraper python
+1. Create a new conda environment, activate it, and import environment.yml
+2. Create a reddit api key:  
+    1. Go to the api request link https://support.reddithelp.com/hc/en-us/requests/  
+    2. Under "What do you need assistance with?" select "API Support and Inquiries"
+    3. Under "From what position are you reaching out for support?" select "I am a developer"
+    4. Under "What is your inquiry?" select "I want to register to use the free tier of the Reddit API"
+    5. Fill in other sections as needed with your own information.
+3. Copy the client id and secret id you received and the redirect uri you used into a file called secrets.json as seen below.
+```json
+{
+    "client_id":"YOUR CLIENT ID",
+    "secret_id":"YOUR SECRET ID",
+    "redirect_uri":"http://localhost:8080",
+    "user_agent":"A reddit scraper for a school project"
+}
 ```
-2. Activate your new conda environment
+4. Change the user agent as you see fit
+5. Run main.py through python using the following syntax:
 ```bash
-conda activate web_scraper
-```
-3. Update your new environment with the requirments.yml:
-```bash
-conda env update --file ./requirments.yml --prune
-```
-4. Run main.py through python using the following syntax:
-```bash
-python ./main.py {website link, default google.com}
+python ./main.py {subreddit link, default reddit.com/r/all}
 ```
--- main.py
+++ main.py
@@ -1,16 +1,80 @@
+import praw
+import regex
+from praw.models import MoreComments
 import argparse
-import requests
+import json
+global maxDepth
+maxDepth = 10
+
+def getComments(link):
+    global reddit
+    sub = reddit.submission(str(link))
+
+    
+    # print(link)
+    postData = []
+    for post in sub.comments:
+        if isinstance(post, MoreComments):
+            continue
+        postData.append(recursiveCommentDownloader(0, post))
+    
+    # print(sub.comments)
+    # try:
+
+    # except:
+    #     raise Exception("Could not find thread")
+    jsonPosts = json.dumps(postData, indent=4)
+    file = open("./output.json", "w")
+    file.writelines(jsonPosts)
+    file.close()
+
+def recursiveCommentDownloader(depth, comment):
+    global maxDepth
+    if(depth > maxDepth):
+        return {
+                'Body': comment.body,
+                'Score': comment.score, #The upvotes minus downvotes
+                'Author': str(comment.author), #The post author
+                'ID': comment.id #The post ID, whether this is needed will be to be determined
+            }
+    myComments = [{
+            'Body': comment.body,
+            'Score': comment.score, #The upvotes minus downvotes
+            'Author': str(comment.author), #The post author
+            'ID': comment.id #The post ID, whether this is needed will be to be determined
+        },[]]
+    for post in comment.replies: 
+        if isinstance(post, MoreComments):
+            continue
+        myComments[1].append(recursiveCommentDownloader(depth + 1, post))
+    # print("returning list of comment")
+    # print(myComments)
+    # If there were no subcomments, return myself
+    
+    return myComments
+
+#Load the keys from a json file in the current directory called secrets.json
+keys = json.load(open("./secrets.json"))
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("link", default="old.reddit.com/r/funny/comments/16brnzb/self_aware", nargs="?")
+parser.add_argument("link", default="https://www.reddit.com/r/funny/comments/16brnzb/self_aware/", nargs="?")
+#Added a default argument just to make debuggin easier
 args = parser.parse_args()
 link = args.link
-if (not (link.startswith("http://") or link.startswith("https://"))):
-    link = "http://" + link
-page = requests.get(link)
-# This will almost always give me an error for too many requests in the output.txt
+reg = r'(reddit.com\/r\/)(.*?)/(comments)/(.*?)/.*'
+subreddit = regex.split(reg, link)[4]
 
-file = open("./output.txt", "w")
-
-file.writelines(page.text)
-file.close()
+# print(subreddit)
+if subreddit != None:
+    print("Looking in id: " + subreddit)
+    global reddit
+    reddit = praw.Reddit(client_id=keys["client_id"],
+                     client_secret=keys["secret_id"],
+                     redirect_uri=keys["redirect_uri"],
+                     user_agent=keys["user_agent"])
+    reddit.read_only = True
+    getComments(subreddit)
+    print("Complete, output in ./output.json")
+else:
+    print("Error: could not find subreddit in link given")
